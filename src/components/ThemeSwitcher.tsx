c34a883c341d71be7154e64fd4f2325cee5b6226
Tinsae <iamtinsae.dev@gmail.com>
2022-09-12 18:39:01 +0300

    feat: theme switcher component

---
+++ src/components/ThemeSwitcher.tsx
@@ -0,0 +1,58 @@
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+
+const ThemeSwitcher = () => {
+  const { resolvedTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  const toggleTheme = () => {
+    if (resolvedTheme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  };
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+  return (
+    <button
+      className="rounded bg-gray-200 p-2 transition hover:ring-2 hover:ring-gray-400 dark:bg-gray-800 dark:ring-gray-600"
+      onClick={toggleTheme}
+    >
+      {resolvedTheme === 'light' ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+          />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+          />
+        </svg>
+      )}
+    </button>
+  );
+};
+
+export { ThemeSwitcher };
