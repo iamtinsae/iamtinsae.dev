c2e48266153ea1b7228999cae0b2872e225d2b74
Tinsae <iamtinsae.dev@gmail.com>
2022-09-16 20:42:52 +0300

    feat(pages): use scroll observer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,13 @@ import '../styles/global.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 
+import { ScrollObserver } from '@/components/ScrollObserver';
+
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider attribute="class">
-    <Component {...pageProps} />
+    <ScrollObserver>
+      <Component {...pageProps} />
+    </ScrollObserver>
   </ThemeProvider>
 );
 
