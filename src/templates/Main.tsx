bf95410fc1b416268a995c24b94b570647208bbd
Tinsae <iamtinsae.dev@gmail.com>
2022-09-16 19:06:55 +0300

    feat(templates): add header

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -1,16 +1,19 @@
 import type { ReactNode } from 'react';
 
+import { Header } from '@/layouts/Header';
+
 type IMainProps = {
   meta: ReactNode;
   children: ReactNode;
 };
 
 const Main = (props: IMainProps) => (
-  <div className="w-full px-1 text-gray-700 antialiased">
+  <div className="w-full px-1 text-gray-700 antialiased dark:text-gray-100">
     {props.meta}
 
     <div className="mx-auto max-w-screen-md">
-      <div className="content py-5 text-xl">{props.children}</div>
+      <Header />
+      <main>{props.children}</main>
     </div>
   </div>
 );
