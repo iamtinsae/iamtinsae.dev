5d8c42a32f199c262790e4e9637042e0674c677e
Tinsae <iamtinsae.dev@gmail.com>
2022-09-20 23:03:51 +0300

    feat(components): convert nav items to react-scroll Link

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,41 +1,36 @@
-import cn from 'classnames';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import dynamic from 'next/dynamic';
+import { Link } from 'react-scroll';
 
 import { ThemeSwitcher } from '@/components/ThemeSwitcher';
 
 type NavItemProps = {
   name: string;
-  href: string;
+  to: string;
 };
 
-const NavItem = (props: NavItemProps) => {
-  const router = useRouter();
-  const isActive = router.asPath === props.href;
-
-  return (
-    <Link href={props.href}>
-      <a
-        className={cn(
-          isActive
-            ? 'font-semibold text-gray-800 dark:text-gray-200'
-            : 'font-normal text-gray-600 dark:text-gray-400',
-          'hidden md:inline-block p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-all'
-        )}
-      >
-        <span>{props.name}</span>
-      </a>
-    </Link>
-  );
-};
+const NavItem = dynamic(
+  () =>
+    Promise.resolve((props: NavItemProps) => {
+      return (
+        <Link href={`#${props.to}`} to={props.to} spy smooth duration={1.4}>
+          <span className="hidden rounded-lg p-1 font-normal text-gray-600 transition-all hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-800 sm:px-3 sm:py-2 md:inline-block">
+            <span>{props.name}</span>
+          </span>
+        </Link>
+      );
+    }),
+  {
+    ssr: false,
+  }
+);
 
 const Header = () => (
   <header className="flex items-center justify-between py-8">
     <nav className="z-50 -ml-[0.75rem]">
-      <NavItem name="Home" href="/" />
-      <NavItem name="Works" href="/works" />
-      <NavItem name="Resume" href="/resume" />
-      <NavItem name="Uses" href="/uses" />
+      <NavItem name="Home" to="about" />
+      <NavItem name="Works" to="works" />
+      <NavItem name="Resume" to="resume" />
+      <NavItem name="Uses" to="uses" />
     </nav>
     <ThemeSwitcher />
   </header>
