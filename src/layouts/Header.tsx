e77fb47358aea60cb200f1d43464f810950e6c2d
Tinsae <iamtinsae.dev@gmail.com>
2022-10-13 09:15:53 +0300

    feat(layouts): create view resume link in header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,5 +1,6 @@
 import cn from 'classnames';
 import dynamic from 'next/dynamic';
+import NextLink from 'next/link';
 import { Link } from 'react-scroll';
 
 import { ThemeSwitcher } from '@/components/ThemeSwitcher';
@@ -7,13 +8,39 @@ import { ThemeSwitcher } from '@/components/ThemeSwitcher';
 type NavItemProps = {
   name: string;
   to: string;
+  isLink?: boolean;
 };
 
 const NavItem = dynamic(
   () =>
     Promise.resolve((props: NavItemProps) => {
+      const LinkComponent = props.isLink ? NextLink : Link;
+      if (props.isLink) {
+        return (
+          <NextLink href={props.to} target="_blank" passHref>
+            <a target={'_blank'}>
+              <span
+                className={cn(
+                  'hidden rounded-lg p-1 transition-all hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-800 sm:px-3 sm:py-2 md:inline-block',
+                  props.to === 'about'
+                    ? 'font-semibold text-gray-900 dark:text-gray-200'
+                    : 'text-gray-600 font-normal'
+                )}
+              >
+                <span>{props.name}</span>
+              </span>
+            </a>
+          </NextLink>
+        );
+      }
       return (
-        <Link href={`#${props.to}`} to={props.to} spy smooth duration={1.4}>
+        <LinkComponent
+          href={`#${props.to}`}
+          to={props.to}
+          spy
+          smooth
+          duration={1.4}
+        >
           <span
             className={cn(
               'hidden rounded-lg p-1 transition-all hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-800 sm:px-3 sm:py-2 md:inline-block',
@@ -24,7 +51,7 @@ const NavItem = dynamic(
           >
             <span>{props.name}</span>
           </span>
-        </Link>
+        </LinkComponent>
       );
     }),
   {
@@ -37,8 +64,8 @@ const Header = () => (
     <nav className="z-50 -ml-[0.75rem]">
       <NavItem name="Home" to="about" />
       <NavItem name="Works" to="works" />
-      {/* <NavItem name="Resume" to="resume" /> */}
       <NavItem name="Uses" to="uses" />
+      <NavItem name="View • Resume" to="/assets/resume.pdf" isLink />
     </nav>
     <ThemeSwitcher />
   </header>
