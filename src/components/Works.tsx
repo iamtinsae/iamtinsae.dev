7ea71a0ac7f3fc5aa410d96bc8d95dd712fe5b3b
Tinsae <iamtinsae.dev@gmail.com>
2022-10-23 18:47:34 +0300

    fix(works): convert h6 to paragraph tag

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -102,9 +102,9 @@ const Works = ({ works }: WorksProps) => {
                   <div className="text-4xl font-semibold tracking-tight md:text-4xl">
                     <WorkLink href={work.link ?? ''}>{work.subTitle}</WorkLink>
                   </div>
-                  <h6 className="mt-2 text-2xl text-gray-200">
+                  <p className="mt-2 text-2xl text-gray-200">
                     {work.description}
-                  </h6>
+                  </p>
                 </WorkDescription>
                 <WorkShowCase
                   progress={progress}
